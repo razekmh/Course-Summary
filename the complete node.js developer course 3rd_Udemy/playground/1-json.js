d0b107998a93a3ae0c481dfe83118e0bfa28ee84
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-15 00:21:10 +0200

    add basic note

--- the complete node.js developer course 3rd_Udemy/playground/1-json.js
+++ the complete node.js developer course 3rd_Udemy/playground/1-json.js
@@ -16,4 +16,7 @@ const fs = require('fs')
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
-console.log(data.title)
+data.name = "Mahmoud"
+data.age = 30 
+const dataString = JSON.stringify(data)
+fs.writeFileSync('1-json.json', dataString)
