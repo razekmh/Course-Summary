216833f8698ddf86adfb3c851ac44fd9f61496cd
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-14 21:24:41 +0200

    yargs first

--- the complete node.js developer course 3rd_Udemy/notes_app/app.js
+++ the complete node.js developer course 3rd_Udemy/notes_app/app.js
@@ -1,10 +1,43 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove'){
-    console.log('Removing note!')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler : function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a note',
+    handler : function () {
+        console.log('Removing the note!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler : function () {
+        console.log('Reading the note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing all notes',
+    handler : function () {
+        console.log('Listing all the notes!')
+    }
+})
+console.log(yargs.argv)
