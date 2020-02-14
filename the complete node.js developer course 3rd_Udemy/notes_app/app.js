179050d0d7b5ece49bfb489d9e3377a150af01cc
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-14 21:12:14 +0200

    add remove

--- the complete node.js developer course 3rd_Udemy/notes_app/app.js
+++ the complete node.js developer course 3rd_Udemy/notes_app/app.js
@@ -1,24 +1,10 @@
-const validator = require('validator')
-
-const log = console.log
-
 const chalk = require('chalk')
+const getNotes = require('./notes.js')
 
-const note_func = require('./notes.js')
+const command = process.argv[2]
 
-const printer = note_func()
-
-console.log(printer)
-
-console.log(validator.isURL('http://razekmh.io'))
-
-console.log(chalk.green.inverse('Success!'))
-
-
-
-// const add_function = require('./utils')
-
-// const sum = add_function(4, -2)
-
-// console.log(sum)
-
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove'){
+    console.log('Removing note!')
+}
