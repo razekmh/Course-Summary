7cbcaeedc8500010a7a193a93a236a808b365014
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 00:31:23 +0200

    weather basic simple

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233'
 
-request({url: url}, (error, respose) =>{
-    const data = JSON.parse(respose.body)
-    console.log(data.currently)
+request({url: url, json:true }, (error, response) =>{
+    console.log(response.body.currently)
 })
