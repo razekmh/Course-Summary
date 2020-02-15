2c7bf82854002a69bd84e2b188fcd203c40c6c48
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 00:35:41 +0200

    weather solution

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('request')
 const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233'
 
 request({url: url, json:true }, (error, response) =>{
-    console.log(response.body.currently)
+    // console.log(response.body.currently)
+    console.log("It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
 })
