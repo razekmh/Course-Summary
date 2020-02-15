a248c3bbada47fa504f3a557a2cba9875cd27807
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-15 02:21:25 +0200

    arrow func intro

--- the complete node.js developer course 3rd_Udemy/notes_app/notes.js
+++ the complete node.js developer course 3rd_Udemy/notes_app/notes.js
@@ -15,9 +15,9 @@ const addNote = function(title, body){
             body: body
         })
         saveNotes(notes)
-        console.log('New note added!')
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     } 
 }
 
