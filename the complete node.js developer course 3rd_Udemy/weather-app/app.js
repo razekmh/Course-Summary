cc73d604960cd7788715932cb56e01b8dd755a31
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 00:42:42 +0200

    summary"
    git push

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233'
+const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233?units=si'
 
 request({url: url, json:true }, (error, response) =>{
     // console.log(response.body.currently)
-    console.log("It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
+    console.log(response.body.daily.data[0].summary+" It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
 })
