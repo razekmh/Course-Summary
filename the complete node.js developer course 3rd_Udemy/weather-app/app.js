c0b073577aeaaca8659766c8935e1eb800d52c5b
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 01:17:01 +0200

    handel location error

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -6,6 +6,8 @@ request({url: url, json:true }, (error, response) =>{
     // console.log(response.body.currently)
     if (error){
         console.log('Unable to connect to weather service!')
+    } else if (response.body.error){
+        console.log('Unable to find location!')
     } else { 
         console.log(response.body.daily.data[0].summary+" It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
     }
