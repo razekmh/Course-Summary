1c871e074c1b644b288fb94366110dcc7125d533
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 02:22:05 +0200

    make geocode callback

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -1,30 +1,52 @@
 const request = require('request')
 
-// const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233?units=si'
+const url  = 'https://api.darksky.net/forecast/9e6569dfe32bc96cea4afda998d4c974/37.8267,-122.4233?units=si'
 
-// request({url: url, json:true }, (error, response) =>{
-//     // console.log(response.body.currently)
+request({url: url, json:true }, (error, response) =>{
+    // console.log(response.body.currently)
+    if (error){
+        console.log('Unable to connect to weather service!')
+    } else if (response.body.error){
+        console.log('Unable to find location!')
+    } else { 
+        console.log(response.body.daily.data[0].summary+" It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
+    }
+})
+
+
+// const weatherUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmF6ZWttaCIsImEiOiJjazV0enlydGgwYmhoM2VvMGFnMHk2aHVnIn0.pRW2wS-NjvG1-Sw4sOoGEg&limit=1'
+
+// request({url: weatherUrl, json:true}, (error, response)=> {
 //     if (error){
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error){
-//         console.log('Unable to find location!')
-//     } else { 
-//         console.log(response.body.daily.data[0].summary+" It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain" )
+//         console.log('Unable to connect to location service!')
+//     } else if (response.body.features.length === 0) {
+//         console.log("No match found!")
+//     } else if (){
+
+//     } else {
+//         console.log(response.body.features[0].center[0] , response.body.features[0].center[1])
 //     }
+    
 // })
 
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoicmF6ZWttaCIsImEiOiJjazV0enlydGgwYmhoM2VvMGFnMHk2aHVnIn0.pRW2wS-NjvG1-Sw4sOoGEg&limit=1'
+    request({url: url, json:true}, (error, response) => {
+        if (error){
+            callback('Unable to connect to location service!')
+        } else if (response.body.features.length === 0){
+            callback("No match found!")
+        } else {
+            callback(undefined, {
+                lat: response.body.features[0].center[1],
+                lon: response.body.features[0].center[0],
+                location : response.body.features[0].place_name
+            })
+        }
+    })
+}
 
-const weatherUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmF6ZWttaCIsImEiOiJjazV0enlydGgwYmhoM2VvMGFnMHk2aHVnIn0.pRW2wS-NjvG1-Sw4sOoGEg&limit=1'
-
-request({url: weatherUrl, json:true}, (error, response)=> {
-    if (error){
-        console.log('Unable to connect to location service!')
-    } else if (response.body.features.length === 0) {
-        console.log("No match found!")
-    } else if (){
-
-    } else {
-        console.log(response.body.features[0].center[0] , response.body.features[0].center[1])
-    }
-    
+geocode('Cairo', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
