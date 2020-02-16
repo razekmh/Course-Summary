647395d3047ab0b787a2702610d0e65372ef539e
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 02:04:50 +0200

    base for callback cha

--- the complete node.js developer course 3rd_Udemy/playground/4-callback.js
+++ the complete node.js developer course 3rd_Udemy/playground/4-callback.js
@@ -1,19 +1,31 @@
-setTimeout(()=>{
-    console.log("Two seconds!")
-}, 2000)
+// setTimeout(()=>{
+//     console.log("Two seconds!")
+// }, 2000)
 
 
-const gecode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
+// const gecode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
 
-        callback(data) 
-    }, 2000)
-}
+//         callback(data) 
+//     }, 2000)
+// }
 
-gecode('city', (collected) => {
-    console.log(collected)
+// gecode('city', (collected) => {
+//     console.log(collected)
+// })
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
