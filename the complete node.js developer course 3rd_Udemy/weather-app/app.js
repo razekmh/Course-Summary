e4792372feac442a1adfadb14a62130ee8280022
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 03:15:58 +0200

    enforce address input

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -2,19 +2,25 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const address = process.argv[2]
 
-geocode(address , (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-    forecast(data.lat, data.lon, (error, forecastData) => {
-        if (error){
+if (!address){
+    console.log("Please provide an address!")
+} else {
+    geocode(address , (error, data) => {
+        if (error) {
             return console.log(error)
         }
+        forecast(data.lat, data.lon, (error, forecastData) => {
+            if (error){
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+          })
+    })
+}
 
-        console.log(data.location)
-        console.log(forecastData)
-      })
-})
+
 
 
 
