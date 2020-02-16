5f8d0a3fc61de133725ee3b84e79f58802dc8f6a
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 03:13:11 +0200

    weather using user input

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -1,7 +1,8 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
 
-geocode('Boston', (error, data) => {
+geocode(address , (error, data) => {
     if (error) {
         return console.log(error)
     }
