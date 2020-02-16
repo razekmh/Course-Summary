4c565b66573f0e472fed8dafa32b618fa39dbf21
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-02-16 03:07:30 +0200

    nested geocode weather app

--- the complete node.js developer course 3rd_Udemy/weather-app/app.js
+++ the complete node.js developer course 3rd_Udemy/weather-app/app.js
@@ -1,13 +1,19 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Cairo', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode('Boston', (error, data) => {
+    if (error) {
+        return console.log(error)
+    }
+    forecast(data.lat, data.lon, (error, forecastData) => {
+        if (error){
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+      })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+
 
