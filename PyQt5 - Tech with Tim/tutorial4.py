76e88ff809034151fb1867eee56d5d269b45f908
Mahmoud Abdelrazek <razekmh@gmail.com>
2020-07-06 16:37:52 +0200

    tut4

--- PyQt5 - Tech with Tim/tutorial4.py
+++ PyQt5 - Tech with Tim/tutorial4.py
@@ -69,7 +69,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.label.setText(_translate("MainWindow", "Hello!"))
         self.menufile.setTitle(_translate("MainWindow", "File"))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
         self.actionNew.setText(_translate("MainWindow", "New"))
